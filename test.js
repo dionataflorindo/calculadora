6a8f82fced2b9551c4046cfabad6fe7526ec4eda
dionata correa <87098743+dionataflorindo@users.noreply.github.com>
2021-08-16 20:27:18 -0300

    Update test.js

--- test.js
+++ test.js
@@ -9,7 +9,6 @@ function criaCalculadora(){
         },
 
         realizarConta(){
-            // @ts-ignore
             let conta = this.display.value;
 
             try{
@@ -18,7 +17,6 @@ function criaCalculadora(){
                     alert('conta inválida')
                 }
 
-                // @ts-ignore
                 this.display.value = conta
             }catch(e){
                 alert('conta inválida')
@@ -27,12 +25,10 @@ function criaCalculadora(){
         },
 
         clearDisplay(){
-            // @ts-ignore
             this.display.value = '';
         },
 
         apagarUm(){
-            // @ts-ignore
             this.display.value= this.display.value.slice(0, -1)
         },
 
@@ -40,20 +36,16 @@ function criaCalculadora(){
             document.addEventListener('click', e => {
                 const el = e.target;
 
-                // @ts-ignore
                 if(el.classList.contains('btn-num')){
-                    // @ts-ignore
                     this.btnParaDisplay(el.innerText)
                 }
-                // @ts-ignore
                 if(el.classList.contains('btn-clear')){
                     this.clearDisplay();
                 }
-                // @ts-ignore
                 if(el.classList.contains('btn-del')){
                     this.apagarUm();
                 }
-                // @ts-ignore
+                
                 if(el.classList.contains('btn-eq')){
                     this.realizarConta();
                 }
@@ -61,7 +53,7 @@ function criaCalculadora(){
 
         },
         btnParaDisplay(valor){
-            // @ts-ignore
+            
             this.display.value += valor
         }
 
